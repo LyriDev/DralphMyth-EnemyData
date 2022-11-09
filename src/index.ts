78e0263dff2206c078339ebe9c88b8836f30191c
†村神† <CA01971172@st.kawahara.ac.jp>
2022-11-09 16:03:31 +0900

    Almost Deleted to change js from ts

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import Module from "module";
-import * as fs from 'fs'
-import {typeJSON} from "./index.d"
-
-const jsonUrl:string="./data.json"
-const json = fs.readFileSync(jsonUrl)
-const data = JSON.parse(json)
-
-window.alert(data)
