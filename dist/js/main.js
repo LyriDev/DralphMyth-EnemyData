c622cbfa7f859dc720f5b1cf5bf9d6a18e4d8222
†村神† <CA01971172@st.kawahara.ac.jp>
2022-11-10 14:54:15 +0900

    set the css style to table

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,41 @@
-console.log("Hello World!");
+const drawArea=document.getElementById("main")
+
+const elementList=["火","氷","風","土","雷","水","光","闇","無"]
+const attackTypeList=["物理","息","魔法"]
+
+function addHtmlTag(tagName,content){//受け取った中身をタグで挟む
+    return `<${tagName}>${content}</${tagName}>`
+}
+function convertNull(value){//値がnullなら"？"として返す
+    if(value===null){
+        return "？"
+    }else{
+        return value
+    }
+}
+
+/* function showAll(data){
+    drawArea.innerHTML=""//描画エリアを初期化する
+    $.each(data.enemy,function(key,value){//データ分だけ処理を行う
+        drawArea.innerHTML+=convertNull(value.name)+":"+convertNull(value.statusEffects.flame)+"<br>"
+    })
+} */
+
+function showName(data){
+    drawArea.innerHTML=""//描画エリアを初期化する
+    $.each(data.enemy,function(key,value){//データ分だけ処理を行う
+        drawArea.innerHTML+=convertNull(value.name)+":"+convertNull(value.statusEffects.flame)+"<br>"
+    })
+}
+
+/* ここから実際の処理 */
+$(function(){
+    $.ajax({
+        url:"./data.json",//jsonファイルの場所
+        dataType:"json",// json形式でデータを取得
+    })
+
+    .done(function(data){
+        //showName(data)//全部のデータを表示する
+    })
+})
