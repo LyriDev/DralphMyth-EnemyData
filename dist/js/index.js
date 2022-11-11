4e3e4bfb2633754310ce8be2e2ecc7ac3fec4deb
†村神† <CA01971172@st.kawahara.ac.jp>
2022-11-11 13:11:11 +0900

    implement showEnemyData()

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,21 +1,50 @@
-const drawArea=document.getElementById("main")//書き換えるHTMLのエリア
-
-
-
-
-
-/* function showAll(data){
-    drawArea.innerHTML=""//描画エリアを初期化する
-    $.each(data.enemy,function(key,value){//データ分だけ処理を行う
-        drawArea.innerHTML+=convertNull(value.name)+":"+convertNull(value.statusEffects.flame)+"<br>"
+function getAllEnemyTag(data){//敵データの全タグ種を取得する関数
+    let enemyTagList=new Array
+    $.each(data.enemy,function(key,value){
+        if(!enemyTagList.includes(value.tag)){
+            enemyTagList.push(value.tag)
+        }
     })
-} */
+    return enemyTagList
+}
 
-function showName(data){
-    drawArea.innerHTML=""//描画エリアを初期化する
-    $.each(data.enemy,function(key,value){//データ分だけ処理を行う
-        drawArea.innerHTML+=convertNull(value.name)+":"+convertNull(value.statusEffects.flame)+"<br>"
-    })
+function showEnemyData(data,filter=null){//敵データの見出しを表示する関数
+    let result=""
+    /* 表の見出しを作成する */
+    const tableHeaderList={//表の見出しリスト
+        name:"名前",
+        level:"レベル",
+        tag:"タグ"
+    }
+    let tableHeaderContent=""//<th>を入れる<tr>要素の中身
+    for(let i in tableHeaderList){//表の見出しを作成する
+        tableHeaderContent+=addHtmlTag("th",tableHeaderList[i])
+    }
+    result+=addHtmlTag("tr",tableHeaderContent)//作成した<th>要素を<tr></tr>で囲んで出力用変数に代入する
+    /* 表の中身を作成する */
+    function getEnemyDataByTag(tagName){//指定されたタグに合致する敵データを取得する関数
+        let enemyDataTr=""//<td>を入れる<tr>要素の中身
+        $.each(data.enemy,function(key,value){
+            if(tagName===value.tag){
+                const enemyDataTd=addHtmlTag("td",value.name)+addHtmlTag("td",value.level)+addHtmlTag("td",value.tag)
+                enemyDataTr+=addHtmlTag("tr",enemyDataTd)//作成した<td>要素を<tr></tr>で囲んで出力する
+            }
+        })
+        return enemyDataTr
+    }
+    let tableDataContent=""//表の中身
+    if(filter===null){//フィルターなしのとき
+        let allEnemyTag=getAllEnemyTag(data)
+        for(let i in allEnemyTag){//タグ毎にデータをまとめて出力する
+            tableDataContent+=getEnemyDataByTag(allEnemyTag[i])
+        }
+    }else{//フィルターありのとき
+        for(let i in filter){//指定されたタグ毎にデータをまとめて出力する
+            tableDataContent+=getEnemyDataByTag(filter[i])
+        }
+    }
+    result+=tableDataContent
+    tableArea.innerHTML=result//表の中身を変更する
 }
 
 /* ここから実際の処理 */
@@ -24,8 +53,7 @@ $(function(){
         url:"./data.json",//jsonファイルの場所
         dataType:"json",// json形式でデータを取得
     })
-
     .done(function(data){
-        //showName(data)//全部のデータを表示する
+        showEnemyData(data)//全部のデータを表示する
     })
 })
--- dist/js/substructure.js
+++ dist/js/substructure.js
@@ -1,17 +1,19 @@
+//const main=document.getElementById("main")//書き換えるHTMLのエリア
+const tableArea=document.getElementById("tableArea")//書き換えるHTMLのエリア
+
 /* 種別リスト */
 const elementList=["火","氷","風","土","雷","水","光","闇","無"]
 const attackTypeList=["物理","息","魔法"]
 
-
 /* 関数等 */
-function addHtmlTag(tagName,content){//受け取った中身をHTMLのタグで挟む
-    return `<${tagName}>${content}</${tagName}>`
-}
-
-function convertNull(value){//値がnullなら"？"として返す
+function convertNull(value){//値がnullなら"？"として返す関数
     if(value===null){
         return "？"
     }else{
         return value
     }
+}
+
+function addHtmlTag(tagName,content){//受け取った中身をHTMLのタグで挟む関数
+    return `<${tagName}>${convertNull(content)}</${tagName}>`
 }
