2e3a8e95025919a32287196b6d05eaa00eeb3aa5
†村神† <CA01971172@st.kawahara.ac.jp>
2022-11-14 15:42:08 +0900

    Create writeToLocal

--- dist/js/index.js
+++ dist/js/index.js
@@ -20,6 +20,10 @@ function convertNull(value,alt="？"){//値がnullなら"？"として返す関�
     }
 }
 
+/* 種別リスト */
+const elementList=["火","氷","風","土","雷","水","光","闇","無"]
+const attackTypeList=["物理","息","魔法"]
+
 /* ページごとに表示するコンテンツを変更するための関数 */
 function updateHTML(data){//HTMLを更新する関数
     switchCssFile()//読み込むCSSファイルを差し替える
--- dist/js/substructure.js
+++
@@ -1,3 +0,0 @@
-/* 種別リスト */
-const elementList=["火","氷","風","土","雷","水","光","闇","無"]
-const attackTypeList=["物理","息","魔法"]
