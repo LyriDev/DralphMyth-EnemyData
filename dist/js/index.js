a2d51ba6ca993e729f5ebb9650e9f457e7db26bb
†村神† <CA01971172@st.kawahara.ac.jp>
2023-03-15 11:27:20 +0900

    fix the url

--- dist/js/index.js
+++ dist/js/index.js
@@ -458,7 +458,7 @@ function updateHeader(data,_page=Page){//ヘッダーを変更する関数
             setUrl("#indexButton",indexUrl)
             setUrl("#editButton",editUrl)
             $(document).on("mousedown","#explanation",function(event){//ホームボタンにクリック処理を適用する
-                window.open("../README.md")
+                window.open("../index.html")
             })
             $(document).on("click","#exportButton",function(){
                 exportEnemyPiece(data.enemy[Index])//出力ボタン処理を適用する
@@ -476,7 +476,7 @@ function updateHeader(data,_page=Page){//ヘッダーを変更する関数
             </div>
             `
             $(document).on("mousedown","#explanation",function(event){//ホームボタンにクリック処理を適用する
-                window.open("../README.md")
+                window.open("../index.html")
             })
             $(document).on("mousedown","#indexButton",function(event){//一覧ボタンにクリック処理を適用する
                 const inputData=getInputData(data)
